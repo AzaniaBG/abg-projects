f0f9e7a3ef40b55b64ffb9e7c20d2e0bf9cca0c4
AzaniaBG <azaniazbaker@gmail.com>
2020-05-13 09:45:22 -0700

    moved ProjectsData file inside Projects folder

---
+++ src/projects/ProjectsData.js
@@ -0,0 +1,29 @@
+//ProjectsData contains a projects KEY with an ARRAY of OBJECT values
+//each OBJECT contains data regarding a project
+const ProjectsData = {
+    "projects": [
+        {
+            id: 1,
+            projectName: "Spanish Quiz App",
+            liveLink: "https://azaniabg.github.io/quiz-app/",
+            image: "../screenshots/Quiz-App/Start_Screen_mobile.jpg",
+            alt: "Mobile screen view of Quiz App's Start Page.",
+            description: "Quiz App that tests intermediate knowledge of the Spanish language.",
+            tech: "1. Ecmascript 5, ES6, 2. jQuery, 3.HTML5, CSS3",
+            repo: "https://github.com/AzaniaBG/quiz-app"
+        },
+        {
+            id: 2,
+            projectName: "Horror House API",
+            liveLink: "https://azaniabg.github.io/Horror-House-V2/",
+            image: "../screenshots/Horror-House/HomeSearchOptions.png",
+            alt: "Scary house in background; search boxes in foreground.",
+            description: "View search results for a horror film, including YouTube trailers, or search for similar horror movies.",
+            tech: "1. Ecmascript 5, ES6, 2. jQuery, 3.HTML5, CSS3",
+            repo: "https://github.com/AzaniaBG/Horror-House-V2"
+        }
+    ]
+
+}
+
+export default ProjectsData 
