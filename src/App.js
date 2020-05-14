ede0dba3b783514dab8b75b7fe2984fa7d462909
AzaniaBG <azaniazbaker@gmail.com>
2020-05-14 11:42:24 -0700

    moved screenshots to src folder

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import ProjectsPage from './projects/ProjectsPage';
 import Context from './Context';
-import HorrorHouseCover from '../src/projects/screenshots/Horror-House/HorrorHouseCover.png';
+// import QuizAppCover from '../src/screenshots/Quiz-App/QuizAppCover';
+import HorrorHouseCover from '../screenshots/Horror-House/HorrorHouseCover.png';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      images: [`${HorrorHouseCover}`]
+      images: {
+        // QuizApp: `${QuizAppCover}`,
+        HorrorHouse: `${HorrorHouseCover}`
+
+      }
     }
   }
   render() {
